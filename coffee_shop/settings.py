9b475c6dcf44ed7a57ba3c8cf5880539743a109d
jarolGabriel <jarolgabriel.jimenez70@gmail.com>
2026-03-15 22:22:04 -0500

    Fix settings.py to use environment variables correctly

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -25,12 +25,15 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%d@fhxutg&a!0n!%oy0#j&9yvzp-oy@&i9^t40v%ui2et9_68a"
+SECRET_KEY = env(
+    "SECRET_KEY",
+    default="django-insecure-%d@fhxutg&a!0n!%oy0#j&9yvzp-oy@&i9^t40v%ui2et9_68a",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
 
 # Application definition
