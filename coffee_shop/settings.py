3d27d35dbb628536f8fbdd2b28922d1e24c70648
jarolGabriel <jarolgabriel.jimenez70@gmail.com>
2026-03-17 21:34:10 -0500

    fix: explicit environment variables for Cloudinary

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -192,9 +192,12 @@ REST_FRAMEWORK = {
 
 # Configuración de Cloudinary
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": env("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": env("CLOUDINARY_API_KEY"),
-    "API_SECRET": env("CLOUDINARY_API_SECRET"),
+    "CLOUD_NAME": env(
+        "CLOUDINARY_CLOUD_NAME", default=os.getenv("CLOUDINARY_CLOUD_NAME")
+    ),
+    "API_KEY": env("CLOUDINARY_API_KEY", default=os.getenv("CLOUDINARY_API_KEY")),
+    "API_SECRET": env(
+        "CLOUDINARY_API_SECRET", default=os.getenv("CLOUDINARY_API_SECRET")
+    ),
 }
-
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
